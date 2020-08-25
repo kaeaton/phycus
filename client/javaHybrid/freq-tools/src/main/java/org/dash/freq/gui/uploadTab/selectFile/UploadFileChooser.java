bbc46ba827951fc1a77510a20524d0338bc85911
Katrina Eaton <katrina.eaton@ucsf.edu>
2020-08-25 06:36:27 -0700

    clean up, add JavaDocs to the Upload File Chooser

--- client/javaHybrid/freq-tools/src/main/java/org/dash/freq/gui/uploadTab/selectFile/UploadFileChooser.java
+++ client/javaHybrid/freq-tools/src/main/java/org/dash/freq/gui/uploadTab/selectFile/UploadFileChooser.java
@@ -1,19 +1,23 @@
 package org.dash.freq.gui.uploadTab.selectFile;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.util.prefs.Preferences;
-
 import javax.swing.JFileChooser;
 
 import org.dash.freq.utilities.Prefs;
 
+/**
+ * Generates the File Chooser for what files or directory to upload.
+ */
 public class UploadFileChooser {
 	private JFileChooser uploadFileChooser = new JFileChooser(Prefs.getUploadPath());
 
 	public UploadFileChooser() { }
 
+	/**
+	 * Generates the File Chooser for what files or directory to upload.
+	 *
+	 * @return A JFileChooser set to accept a file or a directory. 
+	 * It will not allow multiple files or directories to be chosen.
+	 */
 	public JFileChooser getUploadFileChooser() {		
 		uploadFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
