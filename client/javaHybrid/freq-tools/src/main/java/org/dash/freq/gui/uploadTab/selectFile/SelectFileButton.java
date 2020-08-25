8f88f575e7e5e1e8522bcdd7a425ceb4f8262ec1
Katrina Eaton <katrina.eaton@ucsf.edu>
2020-08-25 06:19:58 -0700

    cleanup, add JavaDocs to SelectFileButton

--- client/javaHybrid/freq-tools/src/main/java/org/dash/freq/gui/uploadTab/selectFile/SelectFileButton.java
+++ client/javaHybrid/freq-tools/src/main/java/org/dash/freq/gui/uploadTab/selectFile/SelectFileButton.java
@@ -1,6 +1,6 @@
 package org.dash.freq.gui.uploadTab.selectFile;
 
-// import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,8 +11,13 @@ import javax.swing.JFileChooser;
 import javax.swing.JTextArea;
 
 import org.dash.freq.gui.uploadTab.UploadTabClassInstantiations;
+import org.dash.freq.utilities.AppendText;
 import org.dash.freq.utilities.Prefs;
 
+/**
+ * The button for selecting a file or directory on the upload tab. 
+ * It opens a JFileChooser which is used to select a file or directory.
+ */
 public class SelectFileButton {
 
 	private JFileChooser uploadFileChooser = new UploadFileChooser().getUploadFileChooser();
@@ -20,10 +25,21 @@ public class SelectFileButton {
 	private File selectedFile = null;
 	private String absolutePath, parentFolder, fileName;
 
+	/**
+	 * The class to create and handle the Select File button.
+	 *
+	 * @param fileUpTextArea the text area to print the chosen 
+	 * file or directory to.
+	 */
 	public SelectFileButton(JTextArea fileUpTextArea) {
 		this.fileUploadTextArea = fileUpTextArea;
 	}
 
+	/**
+	 * Creates the button to trigger the JFileChooser to open. 
+	 * 
+	 * @return A JButton, complete with listener.
+	 */
 	public JButton getSelectFileButton() {
 
 		JButton selectFileButton = new JButton("Select File(s)");
@@ -39,33 +55,33 @@ public class SelectFileButton {
 			uploadFileChooser.setVisible(true);
 			int returnValue = uploadFileChooser.showOpenDialog(null);
 			if (returnValue == uploadFileChooser.APPROVE_OPTION) {
+
 				// set the file, get the names necessary
 				selectedFile = uploadFileChooser.getSelectedFile();
 				absolutePath = selectedFile.getAbsolutePath();
 				parentFolder = selectedFile.getParent();
 				fileName = selectedFile.getName();
 				System.out.println(fileName);
-				
-				// fileLocationTextArea.setText( absolutePath );
-				// prefs.put("PHY_INPUT_DIR", absolutePath);
-				// System.out.println(parentFolder);
 
 				Prefs.setUploadPath(absolutePath);
 				uploadFileChooser.setCurrentDirectory(new File(Prefs.getUploadPath()));
 
-				// JTextArea fileUploadTextArea = uploadTabClassInstantiations.getUploadFilesTextAreaInstance();
 				fileUploadTextArea.setText(absolutePath);
-				
-				// if saving receipts to default location, update the textarea
-				// listing where the receipts are being saved
-				// if (defaultUploadReceiptCheckBox.isSelected())
-				// {
-				//	 uploadReceiptTextArea.setText(prefs.get("PHY_DEFAULT_RECEIPT_FOLDER", userDocumentsPath));
-				// }
+
+				// if the absolute path is to a folder, add \*.phycus to the end
+				// to indicate all phycus files in the upload folder
+				if (selectedFile.isDirectory()) {
+					fileUploadTextArea.setText(absolutePath + File.separatorChar + "*.phycus");
+				}
 			}
 		}
 	};
 
+	/**
+	 * The getter method for the file or directory the user has selected.
+	 * 
+	 * @return The selected file or directory.
+	 */
 	public File getTheSelectedFile() {
 		try{
 			return selectedFile;
