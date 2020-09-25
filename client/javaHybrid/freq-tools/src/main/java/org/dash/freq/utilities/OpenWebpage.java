415bebc6384480b7110ca5e073b68673361f516b
Katrina Eaton <katrina.eaton@ucsf.edu>
2020-09-25 10:19:13 -0700

    update javaDocs

--- client/javaHybrid/freq-tools/src/main/java/org/dash/freq/utilities/OpenWebpage.java
+++ client/javaHybrid/freq-tools/src/main/java/org/dash/freq/utilities/OpenWebpage.java
@@ -9,20 +9,23 @@ import java.net.URISyntaxException;
  */
 public class OpenWebpage {
 
-	public OpenWebpage() { }
+	private OpenWebpage() { }
 
 	/**
 	 * Opens the specified URI in the default web browser. 
 	 * 
 	 * @param uri the Uniform Resource Identifier you would like to open.
+	 * @throws URISyntaxException
 	 */
 	public static void openWebpage(URI uri) {
 		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
 			try {
 				desktop.browse(uri);
-			} catch (Exception e) {
-				e.printStackTrace();
+			} catch (URISyntaxException usex) {
+				throw new Error(usex);
+			} catch (Exception ex) {
+				ex.printStackTrace();
 			}
 		}
 	}
