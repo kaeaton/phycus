a35272ee0f594db828b72ee58cd6835545316c71
Katrina Eaton <katrina.eaton@ucsf.edu>
2020-09-26 09:06:32 -0700

    fix error in exception

--- client/javaHybrid/freq-tools/src/main/java/org/dash/freq/utilities/OpenWebpage.java
+++ client/javaHybrid/freq-tools/src/main/java/org/dash/freq/utilities/OpenWebpage.java
@@ -2,7 +2,6 @@ package org.dash.freq.utilities;
 
 import java.awt.Desktop;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * A helper class that allows the program to open webpages in an external browser.  
@@ -15,15 +14,12 @@ public class OpenWebpage {
 	 * Opens the specified URI in the default web browser. 
 	 * 
 	 * @param uri the Uniform Resource Identifier you would like to open.
-	 * @throws URISyntaxException
 	 */
 	public static void openWebpage(URI uri) {
 		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
 			try {
 				desktop.browse(uri);
-			} catch (URISyntaxException usex) {
-				throw new Error(usex);
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
